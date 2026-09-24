52d60f48612019cb598637be6bdb660ac85dcf9d
TaniaBG2 <100503728@alumnos.uc3m.es>
2026-09-24 18:21:14 +0200

    class3.1

--- ism/src/mtf.py
+++ ism/src/mtf.py
@@ -92,6 +92,16 @@ class mtf:
         :return fnAlt: 1D normalised frequencies 2D ALT (f/(1/w))
         """
         #TODO
+        fstepAlt = 1 / nlines / w       # fstepAlt: 333.333333
+        fstepAct = 1 / ncolumns / w     # fstepAct: 222.222222
+
+        eps = 1e-6
+        fAlt = np.arange(-1 / (2 * w), 1 / (2 * w) - eps, fstepAlt)
+        fAct = np.arange(-1 / (2 * w), 1 / (2 * w) - eps, fstepAct)
+
+        [fnAltxx, fnActxx] = np.meshgrid(fnAlt, fnAct, indexing='ij')  # Please use ‘ij’ indexing or you will get the transpose
+        fn2D = np.sqrt(fnAltxx * fnAltxx + fnActxx * fnActxx)
+
         return fn2D, fr2D, fnAct, fnAlt
 
     def mtfDiffract(self,fr2D):
